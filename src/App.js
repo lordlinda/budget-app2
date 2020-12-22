db369b353fdfdcd3a21111ca2cec7131f5e5a9dd
lord <marialindananono@gmail.com>
2020-12-22 06:22:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import AddTransaction from "./components/AddTransaction";
+import Balance from "./components/Balance";
+import ExpenseList from "./components/ExpenseList";
+import Header from "./components/Header";
+import IncomeList from "./components/IncomeList";
+import { GlobalContextProvider } from "./context/GlobalState";
+
+function App() {
+  return (
+    <GlobalContextProvider>
+      <div className="container">
+        <div className="app-wrapper">
+          <Header />
+          <Balance />
+          <IncomeList />
+          <ExpenseList />
+          <AddTransaction />
+        </div>
+      </div>
+    </GlobalContextProvider>
+  );
+}
+
+export default App;
